aeaefd269325eeaaaed0eb2c3bf805f70cca7413
TeganHakim <67878686+TeganHakim@users.noreply.github.com>
2022-07-26 00:00:15 -0400

    Setup for launch

--- jobberCalendar/views.py
+++ jobberCalendar/views.py
@@ -20,10 +20,12 @@ def index(request):
     admin_email = os.getenv('EMAIL')
     admin_password = os.getenv('PASSWORD')
     # Log into Jobber and find elements
-    options = webdriver.ChromeOptions()
-    options.add_argument("--start-maximized");
-    options.add_argument("--headless")
-    driver = webdriver.Chrome(options=options)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
     wait = WebDriverWait(driver, 10)
     driver.get("https://www.getjobber.com/login")
