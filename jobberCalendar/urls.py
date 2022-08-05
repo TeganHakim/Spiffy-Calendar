6ac3618ab1714e10d03ae282471920b10239d5d3
TeganHakim <67878686+TeganHakim@users.noreply.github.com>
2022-08-04 23:07:17 -0400

    Implement loading screen and admin

--- jobberCalendar/urls.py
+++ jobberCalendar/urls.py
@@ -5,5 +5,7 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.loading, name='loading'),
+    path('calendar/', views.index, name='index'),
+    path('home/', views.home, name='home'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
